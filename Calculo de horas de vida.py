913a48dcb9be2c258b9112465480fdf224203545
Jeffer Marcelino <jeffersunde72@gmail.com>
2021-02-02 21:51:13 -0800

    Pedindo os dados do usuario

--- Calculo de horas de vida.py
+++ Calculo de horas de vida.py
@@ -1,1 +1,11 @@
-print("Calculo de Horas de vida").center(30)
+from datetime import datetime
+
+print("Calculo de Horas de vida")
+print("-=" * 15)
+
+ano_nasc = int(input("Digite o seu ano de nascimento: "))
+ano_actual = int(input("Digite o ano actual (999 para pegar o ano do Computador): "))
+if ano_actual == 999:
+    ano_actual = datetime.today().year
+print(ano_nasc)
+print(ano_actual)
